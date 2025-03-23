bdb458d80e55798236795dbaa5941af7d52b9505
Ayushi129 <ayushigupta0704@gmail.com>
2025-03-23 15:28:00 +0530

    toggle light mode

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
+import { useState } from 'react';
 import './App.css'; 
 import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 
 function App() {
+  const [mode, setMode] = useState("light");
+
+  const toggleMode = () => {
+    if(mode === "light"){
+      setMode("dark");
+      document.body.style.backgroundColor = "grey";
+    }
+    else{
+      setMode("light");
+      document.body.style.backgroundColor = "light";
+    }
+  }
+
   return (
     <>
-      <Navbar title="TextUtils" aboutText="About TextUtils"/>
+      <Navbar title="TextUtils" aboutText="About TextUtils" mode={mode} toggleMode={toggleMode}/>
       <div className="container my-4">
-        <TextForm heading="Enter the text to Analyze:"/>
+        <TextForm heading="Enter the text to Analyze:" mode={mode}/>
       {/* <About/> */}
       </div>
     </>
